115d7c670282a0cfad5ec3183b672e4d363314dd
omgbwa-yasse <omgbwayasse@gmail.com>
2025-02-27 19:37:47 -0500

    first commit

---
+++ src/main/java/com/dockeeper/DockeeperApplication.java
@@ -0,0 +1,13 @@
+package com.dockeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockeeperApplication.class, args);
+	}
+
+}
